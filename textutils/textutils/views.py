39ec84853ded90837c70015582f9624597dcf725
Nido89 <syednaveedjalal@gmail.com>
2020-02-09 01:12:03 +0200

    navigation web app to my portfolio url

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -1,9 +1,9 @@
 #I have created this file -Jalal-pwd
 from django .http import HttpResponse
 def index(request):
-    return HttpResponse("<h1>This is Jalal reporting from Django</h1>")
+    return HttpResponse('''<h1>This is Jalal reporting from Django</h1> <a href= "https://github.com/Nido89"> Link to My Github Portfolio <a/>''')
 
 
 def about(request):
-    return HttpResponse("This is about jalal in django python")
+    return HttpResponse("This is about Jalal in django python")
 
