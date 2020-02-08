e257c86efd9dd3436963c25e9e3e6ccb7056986b
Nido89 <syednaveedjalal@gmail.com>
2020-02-08 13:57:47 +0200

    url path names

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -1,7 +1,7 @@
 #I have created this file -Jalal-pwd
 from django .http import HttpResponse
 def index(request):
-    return HttpResponse("This is Jalal reporting from Django")
+    return HttpResponse("<h1>This is Jalal reporting from Django</h1>")
 
 
 def about(request):
