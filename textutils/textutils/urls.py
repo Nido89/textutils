45be7f7f662ac34ae8068b7778ff787b5d2d3a1e
Nido89 <syednaveedjalal@gmail.com>
2020-02-10 09:34:27 +0200

    New Pages with endpoints in url to make a pipeline layout for the app

--- textutils/textutils/urls.py
+++ textutils/textutils/urls.py
@@ -20,6 +20,11 @@ from .import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('removepunc', views.removepunc, name='rempun'),
+    path('capitalitfirst', views.capfirst, name='capfirst'),
+    path('newlineremove', views.newlineremove, name='newlineremove'),
+    path('spaceremover', views.spaceremover, name='spaceremover'),
+    path('charcount', views.charcount, name='charcount'),
     path('about/', views.about, name='about'),
 
 ]
--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -1,7 +1,39 @@
 #I have created this file -Jalal-pwd
 from django .http import HttpResponse
+# def index(request):
+#     return HttpResponse('''<h1>This is Jalal's WEBSITE Portfolio from Django</h1>
+#     <a href= "https://github.com/Nido89"> Link to My Github Portfolio</a>
+#      <p>
+#      <h2>This is Jalal's WEBSITE Portfolio from Django</h2>
+#      <a href= "https://adunosolutions.eu"> Link to My Personnel Company website <a/>
+#                         </p>
+#                         <p>
+#      <h2>This is Jalal's WEBSITE Portfolio from Django</h2>
+#      <a href= "https://artmoc.com"> Link to My Print on demand store <a/>
+#                         </p>
+#                         <p>
+#      <h2>This is Jalal's WEBSITE Portfolio from Django</h2>
+#      <a href= "https://idoam.com"> Link to My directory lsiting site in php <a/>
+#                         </p>
+#      <a/>''')
+
 def index(request):
-    return HttpResponse('''<h1>This is Jalal reporting from Django</h1> <a href= "https://github.com/Nido89"> Link to My Github Portfolio <a/>''')
+    return HttpResponse("<h1>Home</h1>")
+
+def removepunc(request):
+    return HttpResponse("remove punctuations")
+
+def capfirst(request):
+    return HttpResponse("capitalize first letter")
+
+def newlineremove(request):
+    return HttpResponse("Remove new first line")
+
+def spaceremover(request):
+    return HttpResponse("Remove space")
+
+def charcount(request):
+    return HttpResponse("count characters")
 
 
 def about(request):
